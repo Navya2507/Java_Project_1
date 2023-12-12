41f4862f49272a826a38e27cff4e0e9cb6f58707
Navya2507 <152978724+Navya2507@users.noreply.github.com>
2023-12-12 22:10:09 +0530

    Add files via upload

---
+++ Parameters.java
@@ -0,0 +1,11 @@
+package api.endpoints;
+
+public class Parameters {
+	
+	//Send_OTP
+		public static String Username = "9550949255";
+		public static String Login_type = "M";
+		public static String OTP="738760";
+	
+		}
+
